d454078aeb584d15cabf503e6589dc9e78a53e33
Your_Wakanda_Developer_Name_Here <WakandaDeveloper@wakanda.org>
2013-04-20 01:20:24 -0400

    Initial commit

---
+++ CollectionRefresh/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,18 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button4 = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button4.click = function button4_click (event)// @startlock
+	{// @endlock
+		sources.person.collectionRefresh();
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button4", "click", button4.click, "WAF");
+// @endregion
+};// @endlock
